8579ac01681d829e68399580a5b2db01b09a4e14
Josheen Kour <mankaransingh39@gmail.com>
2024-02-26 22:54:08 +0530

    Dashboard Completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
+import TicketCard from "./(components)/TicketCard";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-4">
+      <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
