26d06a37ada5e6157bb2e0f2fff8745e7d26d758
Josheen Kour <mankaransingh39@gmail.com>
2024-02-27 16:43:33 +0530

    Ticket GET and DELETE route set

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,43 @@
 import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+    return res.json();
+  } catch (error) {
+    console.error("Failed to get tickets", error);
+  }
+};
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets();
+
+  const uniqueCategories = [
+    ...new Set(tickets.map(({ category }) => category)),
+  ];
+
   return (
     <div className="p-4">
-      <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div className="mb-4" key={categoryIndex}>
+              <h2>{uniqueCategory}</h2>
+              <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filterTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filterTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
